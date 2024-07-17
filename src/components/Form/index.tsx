11cbaeb053c62a5e84c4b8ed4397431616e6ebfc
Felipe020 <felipesachetto2004@gmail.com>
2024-07-17 18:35:20 -0300

    começou criação de componentes

---
+++ src/components/Form/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Button from '../Button';
+
+class Form extends React.Component{
+    render(): React.ReactNode {
+        return(
+            <form>
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+            </form>
+        )
+    }
+}
