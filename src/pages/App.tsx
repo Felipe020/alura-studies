649e75f1f5b4c4975499d7dd6611dc7dd61c1890
Felipe020 <felipesachetto2004@gmail.com>
2024-07-18 19:19:11 -0300

    criou "esqueleto" do cronometro

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Form from '../components/Form';
 import List from '../components/List';
 import './style.scss'
-
+import Cronometro from '../components/Cronometro';
 
 function App() {
   return (
     <div className="AppStyle">
         <Form />
         <List />
+        <Cronometro />
     </div>
   );
 }
